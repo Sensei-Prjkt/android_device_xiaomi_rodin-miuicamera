d1cba10fa6ee4767c33cf4a3b0b72a1d35b50a8f
Ido <dev.xyzuniverse@gmail.com>
2026-07-04 01:10:52 +0200

    rodin-miuicamera: Configure hypsys stack
    Change-Id: I54366846c1dbaf2c2cc5c071ed4df6cbc0fde104
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,9 @@ CAMERA_PACKAGE_NAME := com.android.camera
 # Properties
 TARGET_SYSTEM_EXT_PROP += $(MIUICAMERA_PATH)/system_ext.prop
 
+# VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(MIUICAMERA_PATH)/framework_compatibility_matrix.xml
+
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/private
